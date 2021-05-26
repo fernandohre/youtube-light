8caac7f271c4875e2591d855a6a90e966ab5f03d
fernandohre <senhor.clinton@gmail.com>
2021-05-25 23:07:38 -0300

    componentizando o ícone e os itens do menu drawer

---
+++ frontend/web/src/components/opcoes-menu.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-list>
+        <v-list-item-group>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-download</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Download Videos</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-cloud-upload</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Upload Videos</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+</template>
+<script>
+export default {
+    
+}
+</script>
