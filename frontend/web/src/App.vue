c1059829ac99525dbbd8cf232b250c3149360a63
fernandohre <senhor.clinton@gmail.com>
2021-05-25 22:17:14 -0300

    layout do youtube light

--- frontend/web/src/App.vue
+++ frontend/web/src/App.vue
@@ -1,31 +1,44 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <v-spacer></v-spacer>
+  <v-app id="inspire">
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Youtube Light</v-toolbar-title>
     </v-app-bar>
 
-    <v-main>
-      <HelloWorld/>
+    <v-navigation-drawer v-model="drawer" fixed temporary>
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-main class="grey lighten-2">
+      <v-container>
+        <v-row>
+          <template v-for="n in 4">
+            <v-col :key="n" class="mt-2" cols="12">
+              <strong>Categoria {{ n }}</strong>
+            </v-col>
+
+            <v-col v-for="j in 6" :key="`${n}${j}`" cols="6" md="2">
+              <v-sheet height="150"></v-sheet>
+            </v-col>
+          </template>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
   },
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      drawer: null,
+    };
+  },
 };
 </script>
