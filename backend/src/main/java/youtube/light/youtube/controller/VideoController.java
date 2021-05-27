a989204e3f1c0c7d99fa06742a88f04d551a6676
fernandohre <senhor.clinton@gmail.com>
2021-05-27 01:06:56 -0300

    chamando serviço no controller

--- backend/src/main/java/youtube/light/youtube/controller/VideoController.java
+++ backend/src/main/java/youtube/light/youtube/controller/VideoController.java
@@ -1,8 +1,23 @@
 package youtube.light.youtube.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import youtube.light.youtube.model.dto.VideoMetadataDto;
+import youtube.light.youtube.service.VideoService;
+
 @RestController
+@RequestMapping(path = "/api/videos")
 public class VideoController {
-    
+    @Autowired
+    private VideoService videoService;
+
+    @GetMapping
+    public ResponseEntity<List<VideoMetadataDto>> getAllVideos() {
+        return ResponseEntity.ok(this.videoService.getAllVideos());
+    }
 }
