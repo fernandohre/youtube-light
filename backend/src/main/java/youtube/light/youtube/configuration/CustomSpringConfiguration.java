ae50f518a44fadcd1e1aea3ba5ad469f9c3af411
fernandohre <senhor.clinton@gmail.com>
2021-05-27 23:47:25 -0300

    implementação inicial do endpoint de upload

---
+++ backend/src/main/java/youtube/light/youtube/configuration/CustomSpringConfiguration.java
@@ -0,0 +1,13 @@
+package youtube.light.youtube.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CustomSpringConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
--- backend/src/main/java/youtube/light/youtube/controller/VideoController.java
+++ backend/src/main/java/youtube/light/youtube/controller/VideoController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import youtube.light.youtube.model.dto.VideoMetadataDto;
 import youtube.light.youtube.service.VideoService;
@@ -20,5 +23,16 @@ public class VideoController {
     @GetMapping
     public ResponseEntity<List<VideoMetadataDto>> getAllVideos() {
         return ResponseEntity.ok(this.videoService.getAllVideos());
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> uploadVideo(@RequestParam("file") MultipartFile file, 
+                                              @RequestParam("description") String description) {
+        //TODO: pegar o arquivo e passar para o service
+        //No service ele deve gerar o id do video, e os metadadas
+        return ResponseEntity.ok(new Object() {
+            public final boolean success = true;
+            public final String videoName = description;
+        });
     }
 }
