da031935a467161ed393869d1fa7ee8d06f397a1
fernandohre <senhor.clinton@gmail.com>
2021-05-27 23:51:36 -0300

    frontend do upload

--- frontend/web/src/components/upload-de-videos.vue
+++ frontend/web/src/components/upload-de-videos.vue
@@ -1,7 +1,47 @@
 <template>
-  <h4>upload de videos</h4>
+  <div class="video-upload">
+    <v-text-field
+      counter="50"
+      label="Descrição"
+      v-model="description"
+    ></v-text-field>
+    <v-file-input
+      name="video-file"
+      counter
+      show-size
+      v-model="fileVideo"
+    ></v-file-input>
+    <v-btn depressed color="primary" @click.stop="uploadVideo"> Enviar </v-btn>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      description: null,
+      fileVideo: null,
+    };
+  },
+  methods: {
+    uploadVideo() {
+      let formData = new FormData();
+      if (this.fileVideo) {
+        formData.append("file", this.fileVideo);
+        formData.append("description", this.description)
+      }
+      axios.post("http://localhost:1010/api/videos", formData)
+        .then((response) => {
+          console.log("Success!");
+          console.log({ response });
+        })
+        .catch((error) => {
+          console.log({ error });
+        });
+      console.log("upload do video...");
+      console.log(formData);
+    },
+  },
+};
 </script>
