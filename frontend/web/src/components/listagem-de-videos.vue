2a730cb080c5c2cb91ab37887cdb587deb790e18
fernandohre <senhor.clinton@gmail.com>
2021-06-09 02:11:49 -0300

    ajustando front para exibir os videos para download, pt1

--- frontend/web/src/components/listagem-de-videos.vue
+++ frontend/web/src/components/listagem-de-videos.vue
@@ -1,11 +1,11 @@
 <template>
   <v-row>
-    <template v-for="n in 4">
-      <v-col :key="n" class="mt-2" cols="12">
+    <template v-for="n in quantidadeDeFileirasHorizontais">
+      <!-- <v-col :key="n" class="mt-2" cols="12">
         <strong>Categoria {{ n }}</strong>
-      </v-col>
+      </v-col> -->
 
-      <v-col v-for="j in 6" :key="`${n}${j}`" cols="6" md="2">
+      <v-col v-for="j in n" :key="`${n}${j}`" cols="6" md="2">
         <v-sheet height="150"></v-sheet>
       </v-col>
     </template>
@@ -13,5 +13,27 @@
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  created() {
+    let _this = this;
+    axios.get("http://localhost:1010/api/videos/findAllVideos")
+         .then(response => {
+           console.log(response);
+           _this.videos = response.data;
+         })
+         .catch(e => console.log("Algo de errado não está certo!", e));
+  },
+  data() {
+    return {
+      videos: []
+    }
+  },
+  computed: {
+    quantidadeDeFileirasHorizontais() {
+      let tamanho = this.videos.length | 4;
+      return tamanho / 4;
+    }
+  }
+};
 </script>
